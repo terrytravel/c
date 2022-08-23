6294b8546cae5a6fa67d39f590c70e8fa8262135
Terry <terry120516@gmail.com>
2022-08-23 13:47:45 +0800

    revers the interger

---
+++ lesson11.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int count = 0;
+    scanf("%d",&count);
+    int digit;
+    int ret =0;
+
+    while (count>0){
+        digit=count%10;
+        printf("%d",digit);
+        ret=ret*10+digit;
+        count/=10;
+    }
+    return 0;
+};
