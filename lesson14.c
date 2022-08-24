849321f0009b3a5d069c806d95c51e2f134c2fdc
Terry <terry120516@gmail.com>
2022-08-24 17:07:54 +0800

    终止循环和前n项求和

---
+++ lesson14.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main()
+{
+    int number;
+    number = 2;
+    int cnt = 0;
+
+    while (cnt < 50)
+    {
+        int i;
+
+        int isprime = 1;
+        for (i = 2; i < number; i++)
+        {
+            if (number % i == 0)
+            {
+                isprime = 0;
+                break;
+            }
+        }
+        if (isprime == 1)
+        {
+            printf("%d\n", number);
+            cnt++;
+        }
+        number++;
+
+    }
+    printf("\n");
+     return 0;
+};
---
+++ lesson15.c
@@ -0,0 +1,18 @@
+//前n项求和(一加一减)
+#include <stdio.h>
+int main(){
+    int n;
+    int i;
+
+    double sum = 0.0;
+    double sign = 1.0;
+
+    scanf("%d",&n);
+
+    for(i=1;i<=n;i++){
+        sum += sign/i;
+        sign = -sign;
+    }
+    printf("f(%d)=%f\n",n,sum);
+    return 0;
+};
